b4a168f542b28669e779abc6e5449907063e9ca0
Yusuf Eren Karabulut <erenikikarabulut@gmail.com>
2024-12-26 19:45:46 +0300

    updated

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://mekanbul-ders-main-1.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
